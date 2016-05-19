c9b8f30abac49441b6bdc5a926e8a6979054c36a
DanielZhu <enterzhu@gmail.com>
2016-05-19 10:00:35 +0800

    Improved the CalcBrain, learn a lot A stack view issue with the final stack op

--- Calculator/Calculator/calcBrain.swift
+++ Calculator/Calculator/calcBrain.swift
@@ -13,14 +13,54 @@ class CalcBrain {
     // The default value
     var accumulator: Double = 0.0
     
-    func performOperator (operate: String) -> Double {
-        if operate == "π" {
-            accumulator = M_PI
-        } else if operate == "e" {
-            accumulator = M_E
-        } else if operate == "√" {
-            accumulator = sqrt(accumulator)
+    private var operations: Dictionary<String, Opreation> = [
+        "π": Opreation.Constant(M_PI),
+        "e": Opreation.Constant(M_E),
+        "√": Opreation.UnaryOperation(sqrt),
+        "+": Opreation.BinaryOperation({ $0 + $1 }),
+        "−": Opreation.BinaryOperation({ $0 - $1 }),
+        "÷": Opreation.BinaryOperation({ $0 / $1 }),
+        "×": Opreation.BinaryOperation({ $0 * $1 }),
+        "±": Opreation.UnaryOperation({ -$0 }),
+        "=": Opreation.Equal
+    ]
+    
+    enum Opreation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equal
+    }
+    
+    var pending: PendingBinaryOperatingInfo?
+    
+    struct PendingBinaryOperatingInfo {
+        var firstOperator: Double
+        var pendingOperating: ((Double, Double) -> Double)
+    }
+    
+    func executeEqual () {
+        if pending != nil {
+            accumulator = pending!.pendingOperating(pending!.firstOperator, accumulator)
+            pending = nil
         }
+    }
+    
+    func performOperator (symbol: String) -> Double {
+        if let operate = operations[symbol] {
+            switch operate {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executeEqual()
+                pending = PendingBinaryOperatingInfo(firstOperator: accumulator, pendingOperating: function)
+            case .Equal:
+                executeEqual()
+            }
+        }
+        
         
         return accumulator
     }
