f2ecd036b358942f50664a2bb1d408ac5f0e2576
DanielZhu <enterzhu@gmail.com>
2016-05-22 23:11:25 +0800

    Calculator: Add feature: dot, %,  AC, update the storyboard

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        updateButtonStyle()
+        super.view.backgroundColor = UIColor.grayColor()
+    }
+    
     var displayValue: Double {
         get {
             return Double(display.text!)!
@@ -36,14 +42,28 @@ class ViewController: UIViewController {
             }
             userInsertDigitFromMiddle = true
         }
-        calcBrain.accumulator = displayValue
-//        print("Touched \(digit) digit")
+        calcBrain.setOperand(displayValue)
     }
     
     @IBAction func performOperator(sender: UIButton) {
         displayValue = calcBrain.performOperator(sender.currentTitle!)
 
         userInsertDigitFromMiddle = false
+    }
+    
+    private func updateButtonStyle () {
+        // loop the subview: stackview
+        for view in self.view.subviews as [UIView] {
+            if let stackView = view as? UIStackView {
+                // loop the each stackview
+                for subStackView in stackView.subviews as [UIView] {
+                    if let btn = subStackView as? UIButton {
+                        btn.layer.borderWidth = 1
+                        btn.layer.borderColor = UIColor.grayColor().CGColor
+                    }
+                }
+            }
+        }
     }
 }
 
--- Calculator/Calculator/calcBrain.swift
+++ Calculator/Calculator/calcBrain.swift
@@ -12,17 +12,33 @@ class CalcBrain {
     
     // The default value
     var accumulator: Double = 0.0
+    var result: Double = 0.0
+    
+    func setOperand (op: Double) {
+        accumulator = op
+
+        result = op ?? 0.0
+    }
+    
+    // Calc between two int and float number
+    // 2, ., 2, 1, 4 -> 2.214
+    func executeDecimal (op1: Double, op2: Double) -> Double {
+        return op1 + op2 * 0.1
+    }
     
     private var operations: Dictionary<String, Opreation> = [
         "π": Opreation.Constant(M_PI),
         "e": Opreation.Constant(M_E),
-        "√": Opreation.UnaryOperation(sqrt),
         "+": Opreation.BinaryOperation({ $0 + $1 }),
         "−": Opreation.BinaryOperation({ $0 - $1 }),
         "÷": Opreation.BinaryOperation({ $0 / $1 }),
         "×": Opreation.BinaryOperation({ $0 * $1 }),
-        "±": Opreation.UnaryOperation({ -$0 }),
-        "=": Opreation.Equal
+        ".": Opreation.BinaryOperation({ $0 + $1 * 0.1 }),
+        "+/-": Opreation.UnaryOperation({ -$0 }),
+        "%": Opreation.UnaryOperation({ $0 / 100 }),
+        "√": Opreation.UnaryOperation(sqrt),
+        "=": Opreation.Equal,
+        "AC": Opreation.ClearAll
     ]
     
     enum Opreation {
@@ -30,6 +46,7 @@ class CalcBrain {
         case UnaryOperation((Double) -> Double)
         case BinaryOperation((Double, Double) -> Double)
         case Equal
+        case ClearAll
     }
     
     var pending: PendingBinaryOperatingInfo?
@@ -39,9 +56,15 @@ class CalcBrain {
         var pendingOperating: ((Double, Double) -> Double)
     }
     
+    func executeClearAll () {
+        accumulator = 0
+        result = 0
+        pending = nil
+    }
+    
     func executeEqual () {
         if pending != nil {
-            accumulator = pending!.pendingOperating(pending!.firstOperator, accumulator)
+            result = pending!.pendingOperating(pending!.firstOperator, accumulator)
             pending = nil
         }
     }
@@ -50,18 +73,19 @@ class CalcBrain {
         if let operate = operations[symbol] {
             switch operate {
             case .Constant(let value):
-                accumulator = value
+                result = value
             case .UnaryOperation(let function):
-                accumulator = function(accumulator)
+                result = function(accumulator)
             case .BinaryOperation(let function):
                 executeEqual()
                 pending = PendingBinaryOperatingInfo(firstOperator: accumulator, pendingOperating: function)
             case .Equal:
                 executeEqual()
+            case .ClearAll:
+                executeClearAll()
             }
         }
         
-        
-        return accumulator
+        return result
     }
 }
